e92ae40aad94121693b55c9d0c7489819caab3c7
rasulbekyahyoxonov <rasulbekyahyoxonov2@gmail.com>
2022-06-27 12:45:29 +0500

    started dtm.uz site

---
+++ javascript.js
@@ -0,0 +1,18 @@
+$('document').ready(function () {
+    $('.bars-ico').click(function () {
+        $('.infoUl').slideToggle(300);
+    });
+
+    // $('.info-a1').mouseenter(function () {
+    //     $('.dropdown-menu').css('display', 'block');
+    // })
+    // $('.info-a1').mouseleave(function () {
+    //     $('.dropdown-menu').css('display', 'none');
+    // })
+    // $('.dropdown-menu').mouseenter(function () {
+    //     $(this).css('display', 'block')
+    // })
+    // $('.dropdown-menu').mouseleave(function () {
+    //     $(this).css('display', 'none')
+    // })
+})
